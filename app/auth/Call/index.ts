47439b1e719cfdb792afa4c479ec1c5d1970053c
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-07-11 18:50:23 +0300

    feat: enhance HTTP request handling with retry logic and token refresh mechanism

--- app/auth/Call/index.ts
+++ app/auth/Call/index.ts
@@ -2,6 +2,8 @@ import { userAuth } from '@/auth'
 import type { FetchOptions, FetchRequest } from 'ofetch'
 import { ofetch } from 'ofetch'
 
+const MAXIMUM_RETRY = 3
+
 export const useHttp = () => {
   // -------------------------- BASE DATA --------------------------
   const config = useRuntimeConfig()
@@ -12,6 +14,7 @@ export const useHttp = () => {
     baseURL: config.public.BASE_URL as string,
     credentials: 'include',
     headers,
+    retry: 2,
 
     // -------------------------- REQUEST --------------------------
     onRequest({ options }) {
@@ -28,6 +31,8 @@ export const useHttp = () => {
 
   // -------------------------- FINAL WRAPPER --------------------------
   return async (request: FetchRequest, options?: FetchOptions) => {
+    let retryCount = 0
+
     try {
       const response = await fetcher.raw(request, options)
       return response._data
@@ -39,22 +44,29 @@ export const useHttp = () => {
         // return logout()
       }
 
-      // Access token expired
-      if (status === 401 || status === 403) {
-        const newToken = await $authService.refreshToken()
+      if (retryCount < MAXIMUM_RETRY) {
+        retryCount += 1
 
-        if (newToken) {
-          options = options || {}
-          options.headers = {
-            ...(options.headers || {}),
-            Authorization: `Bearer ${newToken}`,
+        // Access token expired
+        if (status === 402 || status === 403) {
+          const newToken = await $authService.refreshToken()
+
+          if (newToken) {
+            options = options || {}
+            options.headers = {
+              ...(options.headers || {}),
+              Authorization: `Bearer ${newToken}`,
+            }
+
+            const retryResponse = await fetcher.raw(request, options)
+            return retryResponse._data
+          } else {
+            // return logout()
           }
-
-          const retryResponse = await fetcher.raw(request, options)
-          return retryResponse._data
-        } else {
-          // return logout()
         }
+
+        const retryResponse = await fetcher.raw(request, options)
+        return retryResponse._data
       }
 
       return Promise.reject(error.response?._data || error)
