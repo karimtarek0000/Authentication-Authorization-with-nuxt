45072b5c937fb6d1112ac372f4528cf4bb5eb06b
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-07-15 20:17:05 +0300

    Remove console log from permissions middleware
    - Deleted the console log statement in `app/middleware/permissions.ts` to clean up the code.
    - This change improves performance by reducing unnecessary logging during permission checks.
    - Maintains focus on the core functionality of the middleware without extraneous output.

--- app/middleware/permissions.ts
+++ app/middleware/permissions.ts
@@ -3,7 +3,6 @@ import { $checkPermissions, type PermissionRequirement } from '@/auth'
 export default defineNuxtRouteMiddleware(to => {
   const hasPermissions = $checkPermissions(to.meta.permissions as PermissionRequirement)
 
-  console.log(hasPermissions)
   if (!hasPermissions) {
     return navigateTo('/dashboard')
   }
